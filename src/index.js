e671f2eeed3a58942851b7d1c33dd9664bc1dd0c
iwollmann <iuri.wollmann@concrete.com>
2016-07-22 16:01:37 -0300

    Configuração do roteamento

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-/* eslint-disable no-console */
-console.log('hi');
+import 'babel-polyfill';
+import React from 'react';
+import {render} from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
+
+render(
+    <Router history={browserHistory} routes={routes} />,
+    document.getElementById('app')
+);
