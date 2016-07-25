0aaf8ad1ea10630e982a77eb2b9a54b919968664
iwollmann <iuri.wollmann@concrete.com>
2016-07-25 17:30:56 -0300

    Alteração para o slingshot funcionar

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -12,7 +12,7 @@ export default {
   ],
   target: 'web',
   output: {
-    path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
+    path: path.join(__dirname, '/dist'), // Note: Physical files are only output by the production build task `npm run build`.
     publicPath: '/',
     filename: 'bundle.js'
   },
@@ -25,7 +25,7 @@ export default {
   ],
   module: {
     loaders: [
-      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
       {test: /(\.css)$/, loaders: ['style', 'css']},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
       {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
